f3eb6037c86f05f9e6704dfe2c1de0aa6d51320c
João Oliveira <fc56908@alunos.fc.ul.pt>
2023-04-12 14:15:27 +0100

    su

--- ExRandom/ex.py
+++ ExRandom/ex.py
@@ -3,23 +3,12 @@ import numpy as np
 import sys
 
 # carregar imagens
-
-img_left, img_right = cv2.imread(str(sys.argv[1])), cv2.imread(str(sys.argv[2]))
-
-# converter imagens para escala de cinza
-gray_left, gray_right = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY), cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
-
-# calcular mapa de disparidade
-stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-disparity = stereo.compute(gray_left, gray_right)
-
-# normalizar o mapa de disparidade
-disparity_normalized = cv2.normalize(disparity, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+img_left, img_right = cv2.imread("./imagem3L.png"), cv2.imread("./imagem3R.png")
 
 # criar anaglifo
 height, width = img_left.shape[:2]
 anaglyph = np.zeros((height, width, 3), dtype=np.uint8)
-anaglyph[:, :, 0], anaglyph[:, :, 1], anaglyph[:, :, 2] = img_right[:, :, 2], img_left[:, :, 1], img_left[:, :, 0]
+anaglyph[:, :, 0], anaglyph[:, :, 1], anaglyph[:, :, 2] = img_left[:, :, 1], img_left[:, :, 2], img_right[:, :, 0]
 
 # exibir anaglifo
 cv2.imshow("Anaglyph", anaglyph)
